24aefad9bc62c91b55d98d6788bce3381efb1584
tammy2com <54696658+tammy2com@users.noreply.github.com>
2019-11-09 18:20:32 -0600

    participation 9 and 11 solutions

---
+++ Participation 11/Participation 11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Participation_11
+{
+    class Program
+    {
+        public static bool RepeatedValue(int[] array, int n)
+        {
+            if (array == null || array.Length == 0 || n <= 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 1;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count == n)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+
+        //Test method
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 3, 4, 5, 6, 4, 7, 4, 9 };
+            int n = 3;
+            bool result = RepeatedValue(array, n);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Participation 9/Participation 9/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Participation_9
+{
+    public class Program
+    {
+        public static int[] SumArray(int[] a, int[] b)
+        {
+            int length = a.Length >= b.Length ? a.Length : b.Length;
+            int[] c = new int[length];
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            for (int k = c.Length - 1; k >= 0; k--)
+            {
+                if (a.Length > b.Length)
+                {
+                    c[k] = j < 0 ? a[i] : a[i] + b[j];
+                }
+                else if (a.Length < b.Length)
+                {
+                    c[k] = i < 0 ? b[j] : a[i] + b[j];
+                }
+                else
+                {
+                    c[k] = a[i] + b[j];
+                }
+                i--;
+                j--;
+            }
+
+            return c;
+        }
+
+        static void Main()
+        {
+            int[] a = { 2, 4, 6, 3, 5 };
+            int[] b = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] result = SumArray(a, b);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+        }
+    }
+}
+
+
+            
+
+
+
+            
+        
+        
+        
+        
+        
+        
+        
+         
